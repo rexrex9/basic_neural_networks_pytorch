e9fa5b3dbbb137f7062eaaafeffd304087758ec5
rexrex9 <547102444@qq.com>
2023-12-02 21:36:09 +0800

    update_cnn

--- chapter_llm/rag/read_datas.py
+++ chapter_llm/rag/read_datas.py
@@ -1,16 +1,14 @@
 from os.path import join as osp
 import os
-import json
-import pandas as pd
 from datasets import Dataset
 
 DATAS_DIR = osp(os.path.split(os.path.realpath(__file__))[0],'datas')
 DATAS_JSON = osp(DATAS_DIR,'data.json')
 
 def load():
-    with open(DATAS_JSON,'r',encoding='utf-8') as f:
-        datas=json.load(f)
-    df = pd.DataFrame(datas)
-    dataset = Dataset.from_pandas(df)
+    dataset = Dataset.from_json(DATAS_JSON)
     return dataset
+
+if __name__ == '__main__':
+    print(load())
 
